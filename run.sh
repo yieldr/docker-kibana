371a6355c82ce64173a84b5a8f8c84c3a2bfa5bf
Андрей Корякин <andrey.koryakin@flant.com>
2017-08-10 11:19:05 +0300

    Change server.host from localhost to 0.0.0.0 by default.

--- run.sh
+++ run.sh
@@ -5,8 +5,10 @@ set -e
 KIBANA_CONF_FILE="/opt/kibana-${KIBANA_VERSION}/config/kibana.yml"
 
 KIBANA_ES_URL=${KIBANA_ES_URL:-http://elasticsearch:9200}
+KIBANA_HOST=${KIBANA_HOST:-0.0.0.0}
 
 sed -i "s;.*elasticsearch\.url:.*;elasticsearch\.url: ${KIBANA_ES_URL};" "${KIBANA_CONF_FILE}"
+sed -i "s;.*server\.host:.*;server\.host: ${KIBANA_HOST};" "${KIBANA_CONF_FILE}"
 
 if [ -n "${KIBANA_INDEX}" ]; then
     echo "setting index!"
